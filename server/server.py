43d50f248f6b2f6cd8b16aff1e393db862d6e18d
JAY JAIN <talerajay@gmail.com>
2023-12-19 18:24:04 +0530

    Update server.py

--- server/server.py
+++ server/server.py
@@ -2,12 +2,13 @@ from flask import Flask, request, jsonify
 import util
 app = Flask(__name__)
 
-@app.route('/get_location_names')
+@app.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
-        'locatons' : util.get_location_names()
+        'locations': util.get_location_names()
     })
-    response.headers.add('Access-Control-Allow-Origin','*')
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
     return response
 
 @app.route('/predict_home_price', methods=['GET', 'POST'])
