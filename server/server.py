d0ab41d79c8cec356011ee87d6f4ce3d7289dbd2
JAY JAIN <talerajay@gmail.com>
2023-12-23 21:38:59 +0530

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,18 +1,23 @@
 from flask import Flask, request, jsonify
-import util
 import json
+import numpy as np
+import pickle
 app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def welcome():
+    return 'Welcome To Banglore Price Prediction WEB-APP By :Jay Jain'
 
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
     with open("C:\\JAY TALERA\\DATA SCIENCE\\ML PROJECTS\\Banglore-Real-Estate-Price-Prediction\\server\\artifacts\\columns.json", "r") as f:
         response = json.load(f)['data_columns'][3:]
    
-    loca = jsonify({
+    response = jsonify({
         'locations' : response
     })
-    loca.headers.add('Access-Control-Allow-Origin', '*')
-    return loca
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/predict_home_price', methods=['GET', 'POST'])
 def predict_home_price():
@@ -20,9 +25,28 @@ def predict_home_price():
     location = request.form['location']
     bhk = int(request.form['bhk'])
     bath = int(request.form['bath'])
+    with open("C:\\JAY TALERA\\DATA SCIENCE\\ML PROJECTS\\Banglore-Real-Estate-Price-Prediction\\server\\artifacts\\columns.json", "r") as f:
+        data_columns = json.load(f)['data_columns']
+        locations = data_columns[3:]
+    model = None
+    if model is None:
+        with open('C:\\JAY TALERA\\DATA SCIENCE\\ML PROJECTS\\Banglore-Real-Estate-Price-Prediction\\server\\artifacts\\banglore_home_prices_model.pickle', 'rb') as f:
+            model = pickle.load(f)
+    try:
+        loc_index = data_columns.index(location.lower())
+    except:
+        loc_index = -1
 
+    x = np.zeros(len(data_columns))
+    x[0] = total_sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index>=0:
+        x[loc_index] = 1
+
+    est_price = round(model.predict([x])[0],2)
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+        'estimated_price': est_price
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
